4c710801c77ab86326f82c4855910caa9b97025e
dakshamathematics <dakshamathematics@gmail.com>
2026-04-26 11:09:27 +0530

    Compleated exercise 06_repeatString...

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    if (num<0) {
+        return("ERROR");
+    } else {
+        let outputString = "";
+        let i = 0;
+        while (i < num) {
+            outputString += string;
+            i++;
+    }
+    return(outputString)
+    }
 };
-
+   
 // Do not edit below this line
 module.exports = repeatString;
