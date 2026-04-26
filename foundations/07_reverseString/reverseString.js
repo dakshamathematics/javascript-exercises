c8c0e2146a8b2d3e9fe6d5c6918586e68cc10d8e
dakshamathematics <dakshamathematics@gmail.com>
2026-04-26 12:13:33 +0530

    Completed exercise 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let outputString = "";
+    const length = str.length;
+    let i = length;
+    while ( i > 0) {
+        outputString += str.charAt(i-1)
+        --i;
+    }
+    return (outputString);
 };
 
 // Do not edit below this line
