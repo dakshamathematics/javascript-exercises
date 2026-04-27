233c76f99c21f03bb123e6dc866ce6ef5e91898a
dakshamathematics <dakshamathematics@gmail.com>
2026-04-27 21:21:24 +0530

    Completed Exercise 12_calculator.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return (a+b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return (a-b);
 };
 
-const sum = function() {
-	
+const sum = function(...theArgs) {
+	const myArray = arguments[0];
+  let sum = 0;
+  for (let i=0; i < myArray.length; i++) {
+    sum += myArray[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(...theArgs) {
+	const myArray = arguments[0];
+  let product = 1;
+  for (let i=0; i < myArray.length; i++) {
+    product *= myArray[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	fact = 1;
+  for (let i = 1; i < a+1; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
