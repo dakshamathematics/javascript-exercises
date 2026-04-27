2f88c1e65bdb61c87d39dd8cb1b9c05b21fd17ea
dakshamathematics <dakshamathematics@gmail.com>
2026-04-28 00:16:34 +0530

    Completed Exercise 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (myStr) {
+    const cleanStr = (myStr.replace(/[^a-z0-9]/gi, '')).toLowerCase();
+    let returnValue = true;
+    for (let i = 0; i < cleanStr.length/2; i++) {
+        if (cleanStr[i] === cleanStr[cleanStr.length-i-1]) {
+            continue;
+        } else {
+            returnValue = false;
+            break;
+        }
+    }
+    return returnValue;
 };
 
 // Do not edit below this line
