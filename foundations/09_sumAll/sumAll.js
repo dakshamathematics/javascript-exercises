ab176928281785e88694c575e12f21ffe06a9adf
dakshamathematics <dakshamathematics@gmail.com>
2026-04-27 17:37:46 +0530

    Compleated exercise 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(int1 , int2) {
+    if ((typeof int1 != "number") || (typeof int2 != "number") || (int1 < 0) || (int2 < 0) || (int1 == int2) || (!Number.isInteger(int1)) || (!Number.isInteger(int2))){
+        return "ERROR";
+    } else {
+        return (int1 + int2)*(Math.abs(int1-int2)+1)/2;
+    }
 };
 
 // Do not edit below this line
