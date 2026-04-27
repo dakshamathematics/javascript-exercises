f40b606737cadc3f78c3c7a86e36c3d6099e2d37
dakshamathematics <dakshamathematics@gmail.com>
2026-04-27 10:14:41 +0530

    Completed exercise 08_removeFromArray.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(...theArgs) {
+    let outputArray = [];
+    const myArray = arguments[0];
+
+    for (i = 0; i < myArray.length; i++) {
+        for (j = 1; j < arguments.length; j++) {
+            if (myArray[i] !== arguments[j]) {
+                continue;               
+            } else {
+                break;
+            }
+        }
+        if (j == arguments.length) {
+            outputArray.push(myArray[i]);
+        }
+    }
+    return outputArray;
 };
 
 // Do not edit below this line
