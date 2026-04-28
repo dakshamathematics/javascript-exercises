010578d16c8135c6f1e91b9a4846da3916ce9697
dakshamathematics <dakshamathematics@gmail.com>
2026-04-28 10:03:38 +0530

    Completed exercise 14_fibonacci....

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(myInt) {
+    if (myInt < 0) {
+        return "OOPS";
+    }
+    const retValue = [0,1];
+    let temp = 0;
+    for (let i = 1; i < myInt; i++) {
+        temp = retValue[0];
+        retValue[0] = retValue[1];
+        retValue[1] += temp;
+    }
+    if ( myInt == 0 ) {
+        return 0;
+    } else {
+        return retValue[1];
+    }
 };
 
 // Do not edit below this line
