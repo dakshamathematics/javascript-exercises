eece5081141120ec0c47a80368f7dd11c02de169
dakshamathematics <dakshamathematics@gmail.com>
2026-04-28 10:23:31 +0530

    Completed Exersise 15_getThe Titles...

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(myStructArray) {
+    const outputArray = [];
+    for (let i = 0; i < myStructArray.length; i++) {
+        outputArray.push(myStructArray[i].title);
+    }
+    return outputArray;
 };
 
 // Do not edit below this line
