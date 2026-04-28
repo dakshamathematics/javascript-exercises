a85a33e01acdc6ede57c998a7aff84ba083dec36
dakshamathematics <dakshamathematics@gmail.com>
2026-04-28 16:17:02 +0530

    Completed exercise 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(myStructArray) {
+    const mySA = myStructArray;
+    let maxAgeIndex = 0;
+    let maxAge = 0;
+    const now = new Date().getFullYear();
+    for (let i = 0; i < mySA.length; i++) {
+        if (!Object.hasOwn(mySA[i], 'yearOfDeath')) {
+            mySA[i].yearOfDeath = now;
+        }
+        if ((mySA[i].yearOfDeath - mySA[i].yearOfBirth) > maxAge) {
+                maxAge = (mySA[i].yearOfDeath - mySA[i].yearOfBirth);
+                maxAgeIndex = i;
+        }
+        
+    }
+    return mySA[maxAgeIndex];    
 };
 
 // Do not edit below this line
